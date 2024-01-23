222caec3f59fa68af82f5b4ec8956d47a5443313
rutvik-kuchekar13 <153184823+rutvik-kuchekar13@users.noreply.github.com>
2024-01-23 17:16:36 +0530

    ROCK, PAPER, SCISSOR GAME

---
+++ RSP.py
@@ -0,0 +1,72 @@
+print("!!^^^^WELCOME ROCK,SCISSOR,PAPER GAME^^^^!! ")
+
+import random
+
+list = ["rock", "scissor", "paper"]
+
+# rock vs paper-> paper wins
+# rock vs scissor-> rock wins
+# paper vs scissor-> scissor wins.
+
+
+while True:
+    ccount = 0
+    ucount = 0
+    uc = int(input('''
+Game Start....!
+1 Yes
+2 NO | Exit
+
+          '''))
+    if uc == 1:
+        for a in range(1, 6):
+            userInput = int(input('''
+
+1 Rock
+2 Scissor
+3 Paper
+
+            '''))
+            if userInput == 1:
+                uchoice = "rock"
+            elif userInput == 2:
+                uchoice = "scissor"
+            elif userInput == 3:
+                uchoice = "paper"
+            Cchoice = random.choice(list)
+            if Cchoice == uchoice:
+                print("Computer Value", Cchoice)
+                print("User Value", uchoice)
+                print("Game Tie")
+                ucount = ucount + 1
+                ccount = ccount + 1
+            elif (uchoice == "rock" and Cchoice == "scissor") or (uchoice == "paper"
+                                                                  and Cchoice == "rock") or (
+                    uchoice == "scissor" and Cchoice == "paper"):
+                print("Computer Value", Cchoice)
+                print("User Value", uchoice)
+                print("You Win")
+                ucount = ucount + 1
+            else:
+                print("Computer Value", Cchoice)
+                print("User Value", uchoice)
+                print("Computer Win")
+                ccount = ccount + 1
+
+        if ucount == ccount:
+            print("Final Game Tie....!")
+            print("User Score", ucount)
+            print("Computer Score", ccount)
+        elif ucount > ccount:
+            print("Final You Win The Game....!")
+            print("Computer Lose.!!")
+            print("User Score", ucount)
+            print("Computer Score", ccount)
+        else:
+            print("Final Computer Win The Game....!")
+            print("User Lose.!!")
+            print("User Score", ucount)
+            print("Computer Score", ccount)
+
+    else:
+        break
